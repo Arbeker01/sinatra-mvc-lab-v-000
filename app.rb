927cd03a8b53cd40a0298ca528c57c2b4969bf41
Romeo Gono <romeogono1@aol.com>
2018-11-11 11:18:02 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+      erb :user_input
+    end
+
+  post '/' do
+      @latinized_string = PigLatinizer.new(params[:user_text])
+
+      erb :results
+    end
+
+
 end
