d3ebee884489b3227681cf5ccb2a7f330bc83edf
Romeo Gono <romeogono1@aol.com>
2018-11-11 11:30:30 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,3 +1,8 @@
 class PigLatinizer
 
+  def piglatinize(str_input)
+    output = (str_input.split(" ").length == 1) ? piglatinize_word(str_input) : piglatinize_sentence(str_input)
+    puts output
+    output
+  end
 end
